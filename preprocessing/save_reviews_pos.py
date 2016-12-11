875dbc43168c7e310636e989ff4f4859a73e1a50
sgionescu <sgionescu@gmail.com>
2016-12-10 20:10:18 -0500

    Update save_reviews_pos.py

--- preprocessing/save_reviews_pos.py
+++ preprocessing/save_reviews_pos.py
@@ -23,17 +23,5 @@ for fileName in list_of_files:
     fout.writelines("\n")
     fout.close()
     
-#list_of_files = glob.glob('./test/pos/*.txt')
-#i = 0
-#for fileName in list_of_files:
-#    i = i + 1
-#    print(i)
-#    fin = open( fileName, "r" )
-#    data_list = fin.readlines()
-#    fin.close() # closes file
-#    
-#    fout = open("pos.txt", "a")
-#    fout.writelines(data_list)
-#    fout.writelines("\n")
-#    fout.close()
+
     
