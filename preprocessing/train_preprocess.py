f9981a0c6608e37a003e598f4b3df44bd182da19
sgionescu <sgionescu@gmail.com>
2016-12-10 19:39:52 -0500

    Add files via upload

---
+++ preprocessing/train_preprocess.py
@@ -0,0 +1,106 @@
+"""
+Train convolutional network for sentiment analysis. Based on
+"Convolutional Neural Networks for Sentence Classification" by Yoon Kim
+http://arxiv.org/pdf/1408.5882v2.pdf
+
+For 'CNN-non-static' gets to 82.1% after 61 epochs with following settings:
+embedding_dim = 20          
+filter_sizes = (3, 4)
+num_filters = 3
+dropout_prob = (0.7, 0.8)
+hidden_dims = 100
+
+For 'CNN-rand' gets to 78-79% after 7-8 epochs with following settings:
+embedding_dim = 20          
+filter_sizes = (3, 4)
+num_filters = 150
+dropout_prob = (0.25, 0.5)
+hidden_dims = 150
+
+For 'CNN-static' gets to 75.4% after 7 epochs with following settings:
+embedding_dim = 100          
+filter_sizes = (3, 4)
+num_filters = 150
+dropout_prob = (0.25, 0.5)
+hidden_dims = 150
+
+* it turns out that such a small data set as "Movie reviews with one
+sentence per review"  (Pang and Lee, 2005) requires much smaller network
+than the one introduced in the original article:
+- embedding dimension is only 20 (instead of 300; 'CNN-static' still requires ~100)
+- 2 filter sizes (instead of 3)
+- higher dropout probabilities and
+- 3 filters per filter size is enough for 'CNN-non-static' (instead of 100)
+- embedding initialization does not require prebuilt Google Word2Vec data.
+Training Word2Vec on the same "Movie reviews" data set is enough to 
+achieve performance reported in the article (81.6%)
+
+** Another distinct difference is slidind MaxPooling window of length=2
+instead of MaxPooling over whole feature map as in the article
+"""
+
+import numpy as np
+import data_helpers
+from w2v import train_word2vec
+import csv
+
+from keras.models import Sequential, Model
+from keras.layers import Activation, Dense, Dropout, Embedding, Flatten, Input, Merge, Convolution1D, MaxPooling1D
+
+np.random.seed(2)
+
+# Parameters
+# ==================================================
+#
+# Model Variations. See Kim Yoon's Convolutional Neural Networks for 
+# Sentence Classification, Section 3 for detail.
+
+model_variation = 'CNN-static' #  CNN-rand | CNN-non-static | CNN-static
+print('Model variation is %s' % model_variation)
+
+# Model Hyperparameters
+sequence_length = 56
+embedding_dim = 128          
+filter_sizes = (3, 4)
+num_filters = 150
+dropout_prob = (0.25, 0.5)
+hidden_dims = 150
+
+# Training parameters
+batch_size = 32
+num_epochs = 100
+val_split = 0.1
+
+# Word2Vec parameters, see train_word2vec
+min_word_count = 1  # Minimum word count                        
+context = 10        # Context window size    
+
+# Data Preparatopn
+# ==================================================
+#
+# Load data
+print("Loading data...")
+x, y, vocabulary, vocabulary_inv = data_helpers.load_data()
+print("Vocabulary Size: {:d}".format(len(vocabulary)))
+np.savetxt('data.txt', x) 
+np.savetxt('label.txt', y) 
+
+with open('vocab.csv', 'wb') as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in vocabulary.items():
+        writer.writerow([key, value])
+csv_file.close()
+
+if model_variation=='CNN-non-static' or model_variation=='CNN-static':
+    embedding_weights = train_word2vec(x, vocabulary_inv, embedding_dim, min_word_count, context) 
+    if model_variation=='CNN-static':
+        x = embedding_weights[0][x]
+elif model_variation=='CNN-rand':
+    embedding_weights = None
+else:
+    raise ValueError('Unknown model variation')    
+
+embedding = np.asarray(embedding_weights)
+new_embeding = embedding.reshape(21323*128,1)
+np.savetxt("embedding.txt", np.array(new_embeding))
+
